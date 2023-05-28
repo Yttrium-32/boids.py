868ce1ab928a926d33f599eb2c7b03327d85f209
Yttrium_32 <shresthprasad7@gmail.com>
2023-05-29 00:01:34 +0530

    Added rotation for each generated boid

--- modules/boid.py
+++ modules/boid.py
@@ -1,5 +1,6 @@
 import sys, logging
 import pygame
+from random import randint
 
 class Boid:
     def __init__(self, settings: dict) -> None:
@@ -9,8 +10,14 @@ class Boid:
         except KeyError as e:
             logging.error(f"{e}: No value for ICONPATH found in config.")
             sys.exit()
+        self.boi_list: list[str] = list()
             
-    def renderable(self, postions: list[tuple[int, int]]):
-        for coords in postions:
-            boid_rect = self.boid_surface.get_rect(center = coords)
-            yield self.boid_surface, boid_rect
+    def renderable(self):
+        for boid_rect, boid_rotation in self.boi_list:
+            modified_boid_surface = pygame.transform.rotate(self.boid_surface, boid_rotation)
+            yield modified_boid_surface, boid_rect
+
+    def generate(self, coords: tuple[int, int]):
+        boid_rect = self.boid_surface.get_rect(center = coords)
+        boid_rotation = randint(0,360)
+        self.boi_list.append((boid_rect, boid_rotation))
