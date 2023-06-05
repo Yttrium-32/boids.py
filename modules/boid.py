55123a7accd47cc0ff710279a39a54d9c30ae374
Yttrium_32 <shresthprasad7@gmail.com>
2023-06-05 11:50:23 +0530

    Changed some rotation logic

--- modules/boid.py
+++ modules/boid.py
@@ -14,8 +14,13 @@ class Boid:
         self.boid_list: list[list] = list()
             
     def renderable(self):
-        for boid_rect, boid_rotation in self.boid_list:
-            modified_boid_surface = pygame.transform.rotate(self.boid_surface, boid_rotation)
+        for property_list in self.boid_list:
+            boid_rect: pygame.Rect = property_list[0]
+            boid_rotation = property_list[1]
+            try:
+                modified_boid_surface = pygame.transform.rotate(self.boid_surface, boid_rotation)
+            except pygame.error: 
+                modified_boid_surface = self.boid_surface
             boid_properties = {
                 "surface": modified_boid_surface, 
                 "rectangle": boid_rect, 
@@ -27,24 +32,26 @@ class Boid:
     def generate(self, coords: tuple[int, int]):
         boid_rect = self.boid_surface.get_rect(center = coords)
         boid_rotation = randint(0,360)
-        self.boid_list.append([boid_rect, boid_rotation])
+        self.boid_list.append([boid_rect, boid_rotation, coords])
 
-    def update_properties(self):
+    def update_properties(self, mouse_x: int):
         for i in range(len(self.boid_list)):
             # Changing angle of each boid in boid_list
-            self.boid_list[i][1] = randint(0,360)
+            self.boid_list[i][1] = self.calculate_rotation(self.boid_list[i][2], mouse_x)
+            if self.boid_list[i][1] >= 360:
+                self.boid_list[i][1] = self.boid_list[i][1] - 360
 
-    def calculate_rotation(self, boid_coords: tuple[int,int]) -> int:
+    def calculate_rotation(self, boid_coords: tuple[int,int], mouse_x: int) -> int:
         rotation: int = 0
 
-        mouse_x = pygame.mouse.get_pos()[0]
         x_vector = np.array([mouse_x,0])
         boid_vector = np.array(boid_coords)
 
         dot_product = np.dot(x_vector, boid_vector)
         scalar_prodcut = np.linalg.norm(x_vector) * np.linalg.norm(boid_vector)
 
-        angle_radian = np.arccos(dot_product * scalar_prodcut)
+        angle_radian = np.arccos(dot_product / scalar_prodcut)
         rotation = np.degrees(angle_radian)
 
+        print(f"{boid_vector=}")
         return rotation
