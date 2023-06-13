734c4615ddf17ba3b4e68bead6a95acb9b637e84
Yttrium_32 <shresthprasad7@gmail.com>
2023-06-14 01:38:19 +0530

    Fixed a bug which caused each subsequent text to generate slightly to the right

--- modules/debug.py
+++ modules/debug.py
@@ -1,3 +1,4 @@
+from re import L
 import pygame
 import pygame.freetype
 
@@ -8,9 +9,15 @@ class Debug:
     def __init__(self, message_list: list, screen: pygame.Surface) -> None:
         font = pygame.freetype.Font(None, 12)
         debug_count: int = 0
+        message_coords: list = []
+
         for debug_message in message_list:
             debug_count += 1
-            message_coords: list = [coord * debug_count * 10 for coord in Debug.default_message_coords]
+
+            for _  in range(debug_count):
+                message_coords = [Debug.default_message_coords[0], Debug.default_message_coords[1] * debug_count * 10]
+            print(f"{message_coords=}")
+
             message_surface = font.render(text=debug_message, fgcolor="white", bgcolor="black")
             screen.blit(message_surface[0], message_coords)
 
