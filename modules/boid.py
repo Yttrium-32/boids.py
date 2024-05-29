959888ceaf00c6d55ac2acfa15b2dc71827ca9a3
Shresth Prasad <shresthprasad7@gmail.com>
2024-05-30 00:41:59 +0530

    stop boids from exploding

--- modules/boid.py
+++ modules/boid.py
@@ -5,9 +5,11 @@ from random import uniform
 from typing import Self
 
 class Boid:
-    def __init__(self, perception: int = 10) -> None:
+    def __init__(self, perception: int = 20) -> None:
         window_size_x, window_size_y = pygame.display.get_window_size()
-        self.position = Vector2(window_size_x / 2, window_size_y / 2)
+        start_pos = (uniform(0, window_size_x),
+                     uniform(0, window_size_y))
+        self.position = Vector2(*start_pos)
 
         self.velocity = Vector2(uniform(-0.5, 0.5), uniform(-0.5, 0.5))
         self.acceleration = Vector2()
@@ -26,13 +28,13 @@ class Boid:
                 avg_velocity += other_boid.velocity
 
             try:
-                avg_velocity /= len(local_flock) - 1
+                avg_velocity /= len(local_flock)
             except ZeroDivisionError:
                 # If the only boid in the local_flock is self, don't update avg_velocity
                 ...
 
             steering_force = avg_velocity - self.velocity
-            self.acceleration += steering_force # Since mass of all boids is 1
+            self.acceleration = steering_force # Since mass of all boids is 1
 
     def draw(self, screen: pygame.Surface):
         pygame.draw.circle(screen, "white", self.position, 5)
